8a54b36b71ebb61576b5803a33d942bb40cc628a
TomHyhlik <tomhyhlik@gmail.com>
2024-05-05 13:19:47 +0100

    fix run.sh, there were wrong paths

--- run.sh
+++ run.sh
@@ -1,14 +1,5 @@
+LOGFILE_STDOUT=/home/pi/Repos/ChatHomeAssistant/logs/log_main_stdout.txt
+LOGFILE_STDERR=/home/pi/Repos/ChatHomeAssistant/logs/log_main_stderr.txt
 
-
-LOGFILE_STDOUT=/home/pi/Repos/rpiHomeBot/logs/log_main_stdout.txt
-LOGFILE_STDERR=/home/pi/Repos/rpiHomeBot/logs/log_main_stderr.txt
-
-
-python3 /home/pi/Repos/rpiHomeBot/main.py >> $LOGFILE_STDOUT 2> $LOGFILE_STDERR 
-
-# python3 /home/pi/Repos/rpiHomeBot/main.py &
-
-# sudo python3 /home/pi/Repos/rpiHomeBot/ble_handler.py &
-
-# sudo /home/pi/Repos/ble_scanner/ble_scanner &
+python3 /home/pi/Repos/ChatHomeAssistant/main.py >> $LOGFILE_STDOUT 2> $LOGFILE_STDERR 
 
